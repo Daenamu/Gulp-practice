41b3706e135db5203d5a7b9ae562166525bdd126
Daenamu <56501049+Daenamu@users.noreply.github.com>
2020-07-27 17:17:57 +0900

    sass - compile scss file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,11 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
-import ws from "gulp-webserver"
-import image from "gulp-image"
+import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
     pug: {
@@ -13,30 +16,45 @@ const routes = {
     img: {
         src:"src/img/*",
         dest:"build/img"
+    },
+    scss: {
+        src:"src/scss/style.scss",
+        dest: "build/css",
+        watch: "src/scss/**/*.scss"
     }
 }
 
-const pug = () => gulp
-                    .src(routes.pug.src)
-                    .pipe(gpug())
-                    .pipe(gulp.dest(routes.pug.dest));
+const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
 
 const clean = () => del(["build"])
 
-const webserver = () => 
-    gulp.src("build").pipe(ws({livereload:true, open:true}))
+const img = () => 
+    gulp
+        .src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest))
 
+const styles = () => 
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(routes.scss.dest))
+
+const webserver = () => gulp.src("build").pipe(ws({livereload:true, open:true}))
+    
 const watch = () => {
-    gulp.watch(routes.pug.watch, pug);
-    gulp.watch(routes.img.src, img);
-}
-
-const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest))
-
+        gulp.watch(routes.pug.watch, pug);
+        gulp.watch(routes.img.src, img);
+        gulp.watch(routes.scss.watch, styles);
+    }
 
 const prepare = gulp.series([clean, img])
 
-const assets = gulp.series([pug])
+const assets = gulp.series([pug, styles])
 
 const postDev = gulp.parallel([webserver, watch])
 
